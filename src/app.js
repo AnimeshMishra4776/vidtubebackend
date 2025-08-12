9365a798a25eb8624877829a75379cf25f2ed710
Animesh Mishra <b222008@iiit-bh.ac.in>
2025-08-13 03:52:45 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.urlencoded({extended: true, limit: '18kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+//import routes
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
+
 export default app
